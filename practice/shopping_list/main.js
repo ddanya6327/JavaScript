c3c36419e34b15d86d762892afc6111482c9ef64
ddanya6327 <ddanya6327@gmail.com>
2020-09-16 16:56:01 +0900

    shopping list refactoring

--- practice/shopping_list/main.js
+++ practice/shopping_list/main.js
@@ -17,32 +17,22 @@ function onAdd() {
   input.focus();
 }
 
+let id = 0; // UUID
 function createItem(text) {
   const itemRow = document.createElement("li");
   itemRow.setAttribute("class", "item_row");
+  itemRow.setAttribute("data-id", id);
 
-  const item = document.createElement("div");
-  item.setAttribute("class", "item");
-
-  const name = document.createElement("span");
-  name.setAttribute("class", "item_name");
-  name.innerText = text;
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("class", "item_delete");
-  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-  deleteBtn.addEventListener("click", () => {
-    items.removeChild(itemRow);
-  });
-
-  const itemDivider = document.createElement("div");
-  itemDivider.setAttribute("class", "item_divider");
-
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  itemRow.innerHTML = `
+    <div class="item">
+        <span class="item_name">${text}</span>
+        <button class="item_delete">
+            <i class="fas fa-trash-alt" data-id="${id}"></i>
+        </button>
+    </div>
+    <div class="item_divider"></div>
+  `;
+  id++;
   return itemRow;
 }
 
@@ -53,5 +43,13 @@ addBtn.addEventListener("click", () => {
 input.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
     onAdd();
+  }
+});
+
+items.addEventListener("click", (event) => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item_row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
